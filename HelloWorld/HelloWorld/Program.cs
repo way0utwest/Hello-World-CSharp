a42aa8e3e6bce1d3447ca36f49e90d6a404f3cc1
way0utwest <sjones@dkranch.net>
2015-11-13 10:21:42 -0700

    Added current time

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World");
+            System.Console.WriteLine("Hello, World at " + DateTime.Now);
         }
     }
 }
